b5529c0d3a7169998905993d3e1e5ebc7acbeed7
Pedro Henrique Arthur <pedrohma95@gmail.com>
2024-02-18 08:31:34 -0800

    pushing core

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using piqe.Models.BaseEntity;
+
+namespace piqe.Core
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> QueryAsync(Expression<Func<T, bool>> filter);
+        Task<T> QuerySingleAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByGuidAsync(Guid guid);
+        Task<T> GetByNameAsync(string name);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> SaveAsync();
+    }
+}
